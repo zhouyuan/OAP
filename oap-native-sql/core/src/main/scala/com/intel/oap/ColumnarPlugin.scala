fff105bd52d54a384b781e257e69305b31f9ec6a
Yuan Zhou <yuan.zhou@intel.com>
2020-08-17 10:51:37 +0800

    disable columnar conditioned join
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- oap-native-sql/core/src/main/scala/com/intel/oap/ColumnarPlugin.scala
+++ oap-native-sql/core/src/main/scala/com/intel/oap/ColumnarPlugin.scala
@@ -160,7 +160,7 @@ case class ColumnarPreOverrides(conf: SparkConf) extends Rule[SparkPlan] {
       }
 
     case plan: SortMergeJoinExec =>
-      if (columnarConf.enableColumnarSortMergeJoin) {
+      if (columnarConf.enableColumnarSortMergeJoin && plan.condition == None) {
         val left = replaceWithColumnarPlan(plan.left)
         val right = replaceWithColumnarPlan(plan.right)
         logDebug(s"Columnar Processing for ${plan.getClass} is currently supported.")
